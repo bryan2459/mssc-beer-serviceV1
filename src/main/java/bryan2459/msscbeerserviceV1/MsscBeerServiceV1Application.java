3ec3844cd382fe454d82072ab9816deeb3820d90
bryan2459 <mvs360@gmail.com>
2022-11-13 16:01:04 +1100

    The Initial project add. Closes #1

---
+++ src/main/java/bryan2459/msscbeerserviceV1/MsscBeerServiceV1Application.java
@@ -0,0 +1,13 @@
+package bryan2459.msscbeerserviceV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceV1Application.class, args);
+	}
+
+}
