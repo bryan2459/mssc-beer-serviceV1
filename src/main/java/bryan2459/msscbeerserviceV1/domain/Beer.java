73f08c32aaab4e6b8a24caba5efa06f6650d5747
bryan2459 <mvs360@gmail.com>
2022-11-27 12:38:39 +1100

    adding beer entity. - closes #5

---
+++ src/main/java/bryan2459/msscbeerserviceV1/domain/Beer.java
@@ -0,0 +1,49 @@
+package bryan2459.msscbeerserviceV1.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition="varchar",updatable=false,nullable=false)
+    private UUID id;
+
+    @Version
+    private long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createDate;
+
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long UDC;
+    private BigDecimal price;
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+
+
+
+}
